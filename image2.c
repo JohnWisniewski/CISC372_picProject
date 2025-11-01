368822aa38ca6f12faa56db62c830d09d1fe95b1
JohnWisniewski <john@jt.localdomain>
2025-11-01 14:29:48 -0400

    matrix decleration

--- image2.c
+++ image2.c
@@ -21,7 +21,7 @@ static inline uint8_t clamp_u8(double v) {
 
 // An array of kernel matrices to be used for image convolution.
 // The indexes match the enumeration from the header file.
-Matrix algorithms[] = {
+Matrix algorithms[][3][3] = {
     { { 0,-1, 0},{-1, 4,-1},{ 0,-1, 0} },           // EDGE
     { { 0,-1, 0},{-1, 5,-1},{ 0,-1, 0} },           // SHARPEN
     { {1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0} }, // BLUR
